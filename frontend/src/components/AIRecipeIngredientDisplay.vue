7d772df255923f9d099669bb930e49ea01ffcea2
Ming Bao <mingbao2891@gmail.com>
2025-08-03 15:50:36 +1200

    Generate page done

--- frontend/src/components/AIRecipeIngredientDisplay.vue
+++ frontend/src/components/AIRecipeIngredientDisplay.vue
@@ -22,7 +22,7 @@
 </script>
 
 <template>
-  <div>
+  <div class="ingredients-block">
     <h2>Ingredients:</h2>
     <ul>
       <li v-for="(ingredient, i) in ingredients" :key="i">
@@ -50,5 +50,14 @@
   }
   li {
     margin-bottom: 0.2rem;
+  }
+  .ingredients-block {
+    background: #232323;
+    color: #fff;
+    border-radius: 16px;
+    box-shadow: 0 2px 12px rgba(0,0,0,0.12);
+    padding: 1.5rem 1.2rem 1.2rem 1.2rem;
+    margin-bottom: 1.5rem;
+    width: 100%;
   }
 </style>
